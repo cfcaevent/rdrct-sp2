c800bb6641991f97eba244a54564facdb70c534e
cfcaevent <cfca.event@gmail.com>
2025-04-02 12:41:08 +0000

    Update index.php

--- index.php
+++ index.php
@@ -52,11 +52,11 @@
         //var phone_number = '919 610 587';
         var phone_number = '910 971 803';
 </script>
-<!-- Hotjar Tracking Code for ar-amordiario.online -->
+<!-- Hotjar Tracking Code for checkes -->
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5149129,hjsv:6};
+        h._hjSettings={hjid:5358886,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
